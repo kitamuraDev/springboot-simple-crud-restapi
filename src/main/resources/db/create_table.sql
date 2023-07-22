28c227897d196473263994943871d37f48cd4f67
kitamuraDev <masato1999824@icloud.com>
2023-07-23 00:16:45 +0900

    :bento: DDLを追加

---
+++ src/main/resources/db/create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS characters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+-- DROP TABLE characters;
---
+++ src/main/resources/db/insert-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO characters (name) VALUES
+('桜島 麻衣'),
+('梓川 咲太'),
+('梓川 花楓');
